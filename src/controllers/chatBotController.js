3eaa7afc554e5c5aa2f90e68f1bbc56f4ca0a459
jean victor krauth <jeanvictorkrauth@MacBook-Air-de-jean-2.local>
2021-03-24 23:05:08 +0100

    tamere

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -75,7 +75,7 @@ let getWebhook = (req,res) =>{
 function handleMessage(sender_psid, received_message) {
   console.log("-----------");
   console.log("-----------");
-  console.log(received_message.nlp.entities["datetime"]);
+  console.log(received_message.nlp.entities.values);
   console.log("-----------");
   console.log("-----------");
   console.log(received_message.nlp.traits);
